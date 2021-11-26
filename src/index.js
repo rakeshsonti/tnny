ca9a4c7ab12531d48b18f20670d1bbaa80cb6242
rambhajansonti <23256559@mahindra.com>
2021-11-26 19:37:40 +0530

    unique hashcode genrated

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 app.use(
-    cors({
-        credentials: true,
-       origin: "*",
-    })
+    cors()
  );
  app.use(express.json());
  const mongoose = require("mongoose");
@@ -15,37 +12,87 @@ app.use(
     useUnifiedTopology: true,
  });
  const userSchema = new mongoose.Schema({
-    username: String,
-    url: String,
+    url:String,
+    newUrl: String,
+    hashcode:String,
     createdDateTime: String
  });
  const user = db.model("Users", userSchema);
- app.post("/getUrl",async (req,res)=>{
+ const crypto = require("crypto");
+const randomValueHex1=function (len) {
+    return crypto.randomBytes(Math.ceil(len/2))
+        .toString('hex') // convert to hexadecimal format
+        .slice(0,len).toUpperCase();   // return required number of characters
+}
+const randomValueHex2=function (len) {
+    return crypto.randomBytes(Math.ceil(len/2))
+        .toString('hex') // convert to hexadecimal format
+        .slice(0,len).toLowerCase();   // return required number of characters
+}
+const generateUniqueKey=function(){
+    const str = randomValueHex2(4)+"-"+randomValueHex1(4);
+    console.log(str);
+    return str;
+}
+ app.get("/getUrl",async (req,res)=>{
      console.log("get url")
-     const {username,url}=req.query;
-     console.log("get url",url,username)
-     const newUser = new user({
-        username,
-        url,
-        createdDateTime:new Date().toLocaleString(),
-     });
-     await newUser.save();
-     res.send(url);
+     const {url}=req.query;
+     if(url===undefined||url===null){
+        res.send({url:"Invalid url"});
+    }else{
+        const curr_url = await user.find({
+            url
+         },{newUrl:1,_id:0});
+         if(curr_url[0]!==null && curr_url[0]!=undefined){
+            res.send({url:curr_url[0].newUrl});
+         }else{
+            let hashcode=null;
+            let isUnique=false;
+            while(!isUnique){
+                hashcode=await generateUniqueKey();
+                const curr_hashcode = await user.find({
+                    hashcode
+                 },{_id:1});
+                 console.log(curr_hashcode)
+                 if(curr_hashcode[0]===null || 
+                    curr_hashcode[0]===undefined || curr_hashcode[0]==="" ||curr_hashcode[0]==[] ){
+                    isUnique=true;
+                    break;
+                 }
+            }
+            let newGenUrl=req.protocol+"://"+req.hostname+":"+3000+"/"+hashcode;
+            console.log(newGenUrl);
+             const newUser = new user({
+                url,
+                newUrl:newGenUrl,
+                hashcode:hashcode,
+                createdDateTime:new Date().toLocaleString(),
+             });
+             await newUser.save();
+             res.send({url:newGenUrl});
+         }
+    }
  })
+ 
 app.get("/",async (req,res)=>{
     console.log("server is heated");
-    const {username}=req.query;
-    const url = await user.find({
-        username: username
+    const {newUrl}=req.query;
+    
+    if(newUrl===undefined||newUrl===null){
+        res.send({url:"Invalid url"});
+    }else{
+    const curr_url = await user.find({
+        newUrl
      },{url:1,_id:0});
-     if(url!==null && url!=undefined){
-        // res.status(301).send({"Location":url[0].url});
-        res.writeHead(301,{"Location":url[0].url})
+     if(curr_url[0]!==null && curr_url[0]!=undefined){
+         console.log(curr_url)
+        res.writeHead(301,{"Location":curr_url[0].url})
         res.end();
      }else{
          res.send({url:"not found"});
      }
     // res.send("server is working fine");
+    }
 })
 
 
